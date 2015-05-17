8d30e756e7a4c967bbc59548f66dc670de0a2ed1
nulltitleexception <github@proffitt.in>
2015-05-17 14:27:44 -0400

    3 tests added

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <chrono>
+
+#define len 10000000
+int main()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    std::cout << "Program Start" << std::endl;
+    std::cout << "Creating data structures..." << std::endl;
+    int* data = new int[len];
+    int* pointerData = new int[len];
+    int** dataP = new int*[len];
+    int* missData = new int[len];
+    int** DataM = new int*[len];
+    std::cout << "Formatting data structures..." << std::endl;
+    for (int i = 0; i < len; i++){
+        data[i] = 1;
+        pointerData[i] = 1;
+        dataP[i] = &pointerData[i];
+        missData[i] = 1;
+        DataM[i] = &missData[((i % len)/2) + ((len/2) * (i%2))];
+    }
+    // test 0
+    std::cout << "Test 0 - Packed Array: Initialized at t=0" << std::endl;
+    auto init0 = std::chrono::high_resolution_clock::now();
+    for (int a = 0; a < 10; a++){
+    for (int i = 0; i < len; i++){
+            data[i] += 3;
+    }}
+    auto elapsed0 = std::chrono::high_resolution_clock::now() - init0;
+    long long microseconds0 = std::chrono::duration_cast<std::chrono::microseconds>(elapsed0).count();
+    std::cout << "Test 0 - Packed Array: Terminated at t=" << microseconds0 << std::endl;
+    //test 1
+    std::cout << "Test 1 - Pointer Traversal: Initialized at t=0" << std::endl;
+    auto init1 = std::chrono::high_resolution_clock::now();
+    for (int a = 0; a < 10; a++){
+    for (int i = 0; i < len; i++){
+            *dataP[i] += 3;
+    }}
+    auto elapsed1 = std::chrono::high_resolution_clock::now() - init1;
+    long long microseconds1 = std::chrono::duration_cast<std::chrono::microseconds>(elapsed1).count();
+    std::cout << "Test 1 - Pointer Traversal: Terminated at t=" << microseconds1 << std::endl;
+    //test 2
+    std::cout << "Test 2 - Cache Miss: Initialized at t=0" << std::endl;
+    auto init2 = std::chrono::high_resolution_clock::now();
+    for (int a = 0; a < 10; a++){
+    for (int i = 0; i < len; i++){
+            *DataM[i] += 3;
+    }}
+    auto elapsed2 = std::chrono::high_resolution_clock::now() - init2;
+    long long microseconds2 = std::chrono::duration_cast<std::chrono::microseconds>(elapsed2).count();
+    std::cout << "Test 2 - Cache Miss: Terminated at t=" << microseconds2 << std::endl;
+    return 0;
+}
