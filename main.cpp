1a892bbea87a0a966e5056b06ecd52950c7a505d
Zackary Corbett <hello@zyber17.com>
2015-05-17 14:42:12 -0400

    Added spaces because I'm not a barbarian

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <chrono>
 
-#define len 10000000
+#define len 1048576
 int main()
 {
     auto start = std::chrono::high_resolution_clock::now();	
+
     std::cout << "Program Start" << std::endl;
     std::cout << "Creating data structures..." << std::endl;
+
     int* data = new int[len];
     int* pointerData = new int[len];
     int** dataP = new int*[len];
     int* missData = new int[len];
     int** DataM = new int*[len];
+
     std::cout << "Formatting data structures..." << std::endl;
     for (int i = 0; i < len; i++){
         data[i] = 1;
@@ -20,6 +23,7 @@ int main()
         missData[i] = 1;
         DataM[i] = &missData[((i % len)/2) + ((len/2) * (i%2))];
     }
+
     // test 0
     std::cout << "Test 0 - Packed Array: Initialized at t=0" << std::endl;
     auto init0 = std::chrono::high_resolution_clock::now();
@@ -27,9 +31,12 @@ int main()
     for (int i = 0; i < len; i++){
             data[i] += 3;
     }}
+
     auto elapsed0 = std::chrono::high_resolution_clock::now() - init0;
     long long microseconds0 = std::chrono::duration_cast<std::chrono::microseconds>(elapsed0).count();
     std::cout << "Test 0 - Packed Array: Terminated at t=" << microseconds0 << std::endl;
+
+
     //test 1
     std::cout << "Test 1 - Pointer Traversal: Initialized at t=0" << std::endl;
     auto init1 = std::chrono::high_resolution_clock::now();
@@ -37,9 +44,12 @@ int main()
     for (int i = 0; i < len; i++){
             *dataP[i] += 3;
     }}
+
     auto elapsed1 = std::chrono::high_resolution_clock::now() - init1;
     long long microseconds1 = std::chrono::duration_cast<std::chrono::microseconds>(elapsed1).count();
     std::cout << "Test 1 - Pointer Traversal: Terminated at t=" << microseconds1 << std::endl;
+
+    
     //test 2
     std::cout << "Test 2 - Cache Miss: Initialized at t=0" << std::endl;
     auto init2 = std::chrono::high_resolution_clock::now();
